6261e71d74e163f964f829a5e9a086f8e892e6ac
JooMin Choi <joominchoi23@gmail.com>
2024-01-05 11:45:34 +0000

    Add function that uses recursion to return Fibonacci sequence

--- fibonacci.js
+++ fibonacci.js
@@ -10,4 +10,21 @@ function fibs(n) {
   return myArray;
 }
 
-console.log(fibs(6))
+function fibsRec(n) {
+  if (n <= 0) {
+      console.log("Invalid input. Please provide a positive integer for Fibonacci sequence.");
+      return ["Please don't attempt to break me"];
+  } else if (n === 1) {
+      return [0];
+  } else if (n === 2) {
+      return [0, 1];
+  } else {
+      const prevSequence = fibsRec(n - 1);
+      const lastTwoNumbers = [prevSequence[prevSequence.length - 2], prevSequence[prevSequence.length - 1]];
+      const sumOfLastTwo = lastTwoNumbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+      return [...prevSequence, sumOfLastTwo];
+  }
+}
+
+console.log(fibs(5))
+console.log(fibsRec(10))
